f212d6ff7267b3e8170bc644eaa92999bb6a7f41
Joe Pea <joe@trusktr.io>
2020-02-10 13:15:19 -0800

    initial landing page for Lume for Desktop. Needs mobile responsiveness

--- client/entry.js
+++ client/entry.js
@@ -2,14 +2,16 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import '../imports/routes'
 import {Motor, useDefaultNames} from 'infamous'
+import TWEEN from '@tweenjs/tween.js'
 
 // tell infamous to define the default set of custom elements (all of them)
 useDefaultNames()
 
-Tracker.autorun(function() {
+Tracker.autorun(() => {
 	document.title = Session.get('appTitle')
 })
 
+// TODO find a place for these demos
 const demos = _.shuffle([
 	//'//dl.dropboxusercontent.com/u/2790629/famin/demo2/index.html?50', // FIXME in Safari 9, or remake
 	//'http://devmagnet.net/boxer/demo/examples/rotate-180-nodes/', // no HTTPS, or remake
@@ -28,149 +30,151 @@ const demos = _.shuffle([
 	'//trusktr.io/rainbowTriangles',
 ])
 
-// copied from https://codepen.io/trusktr/live/JMMXPB
-class MorphingColorSpiral extends React.Component {
-	constructor() {
-		super()
+// TODO fix bug where setting Z rotation back to zero doesn't work (while X and Y are zero).
 
-		this.state = {
-			rotation: 0,
-		}
+class Cube extends React.Component {
+	render = () => {
+		const props = {...this.props}
+		const size = props.size
+		delete props.size
 
-		const nodes = Array(400).fill(0)
-
-		this.staticInnerScene = (
-			<i-scene id="spiral-inner-scene">
-				{nodes.map((n, i) => (
-					<i-node key={i} size="0 0 0" align="0.5 0.5" rotation={[0, 0, i * 10]}>
-						<i-node
-							size={[50 - (i % 50), 50 - (i % 50), 0]}
-							mount-point="0.5 0.5"
-							position={[0, i * 2, 0]}
-							style={{
-								background: `hsl( ${(i * 2) % 360}, 90%, 78%)`,
-								borderRadius: `${i % 50}px`,
-							}}
-						></i-node>
-					</i-node>
-				))}
-			</i-scene>
-		)
+		// prettier-ignore
+		return (
+            <i-node ref="cubeContainer" {...props}>
+                <i-node style={{...s.cubeSide, ...s.front}}  position={[0, 0, size/2]}  rotation="0 0 0"   size={[size, size, 0]} mount-point="0.5 0.5" align="0.5 0.5"></i-node>
+                <i-node style={{...s.cubeSide, ...s.back}}   position={[0, 0, -size/2]} rotation="0 180 0" size={[size, size, 0]} mount-point="0.5 0.5" align="0.5 0.5"></i-node>
+                <i-node style={{...s.cubeSide, ...s.left}}   position={[-size/2, 0, 0]} rotation="0 -90 0" size={[size, size, 0]} mount-point="0.5 0.5" align="0.5 0.5"></i-node>
+                <i-node style={{...s.cubeSide, ...s.right}}  position={[size/2, 0, 0]}  rotation="0 90 0"  size={[size, size, 0]} mount-point="0.5 0.5" align="0.5 0.5"></i-node>
+                <i-node style={{...s.cubeSide, ...s.top}}    position={[0, -size/2, 0]} rotation="-90 0 0" size={[size, size, 0]} mount-point="0.5 0.5" align="0.5 0.5"></i-node>
+                <i-node style={{...s.cubeSide, ...s.bottom}} position={[0, size/2, 0]}  rotation="90 0 0"  size={[size, size, 0]} mount-point="0.5 0.5" align="0.5 0.5"></i-node>
+            </i-node>
+        )
 	}
 
-	render() {
-		return (
-			<i-scene id="spiral-outer-scene" style={{background: '#333'}}>
-				<i-node size="1630 1630" align="0.5 0.5" mount-point="0.5 0.5" rotation={[0, 0, this.state.rotation]}>
-					{this.staticInnerScene}
-				</i-node>
-			</i-scene>
-		)
+	componentDidMount() {
+		this.props.containerRef && this.props.containerRef(this.refs.cubeContainer)
+	}
+}
+
+class App extends React.Component {
+	state = {
+		cubeRotation: 0,
 	}
 
 	componentDidMount() {
 		Motor.addRenderTask(() => {
-			this.setState({
-				rotation: this.state.rotation - 9.8,
-			})
+			const logo = this.refs.logo
+			if (logo) logo.rotation.y++
 		})
 	}
+
+	containerRef = cubeNode => {
+		const tween = new TWEEN.Tween({r: 0})
+
+		Motor.addRenderTask(t => {
+			cubeNode.rotation.y++
+		})
+	}
+
+	componentDidMount() {}
+
+	render = () => (
+		<i-scene ref="scene">
+			<i-node size-mode="proportional proportional" size="1 1 0">
+				<i-node headerBar size-mode="proportional literal" size="1 148 0">
+					<div
+						style={{
+							display: 'flex',
+							height: '100%',
+							alignItems: 'center',
+							paddingLeft: 112,
+							paddingRight: 132,
+						}}
+					>
+						<img
+							src="/images/logo.svg"
+							style={{
+								width: 40,
+								height: 40,
+								objectFit: 'fill',
+
+								// push everything else to the right side of the header
+								marginRight: 'auto',
+							}}
+						/>
+
+						<a style={s.menuLink} href="https://infamous.io/docs/index.html">
+							Documentation
+						</a>
+						<a style={s.menuLink} href="https://infamous.io/docs/examples.html">
+							Examples
+						</a>
+						<a style={s.menuLink} href="https://github.com/infamous/infamous">
+							GitHub
+						</a>
+					</div>
+				</i-node>
+
+				<i-node
+					wordmark
+					size-mode="proportional proportional"
+					size="0.50 0 0"
+					mount-point="0.5 0.5"
+					align="0.5 0.5"
+				>
+					<img
+						src="/images/logo-wordmark.svg"
+						style={{
+							transform: 'translateY(-50%)',
+							width: '100%',
+							height: 'auto',
+							objectFit: 'fill',
+						}}
+					/>
+				</i-node>
+
+				<Cube
+					containerRef={this.containerRef}
+					size="200"
+					align="0.5 0.5"
+					position="0 0 -200"
+					rotation="45 45 45"
+				/>
+			</i-node>
+		</i-scene>
+	)
 }
+
+/** @type {{[k: string]: React.CSSProperties}} */
+const styles = {
+	menuLink: {
+		textTransform: 'uppercase',
+		marginLeft: 91,
+		letterSpacing: '0.165em',
+		color: 'white',
+		transform: 'translateY(15px)',
+	},
+
+	cubeSide: {
+		// background: 'deeppink',
+	},
+
+	// prettier-ignore
+	...{
+        front: { background: 'url(/images/cube/front.svg)' },
+        back: { background: 'url(/images/cube/back.svg)' },
+        left: { background: 'url(/images/cube/left.svg)' },
+        right: { background: 'url(/images/cube/right.svg)' },
+        top: { background: 'url(/images/cube/top.svg)' },
+        bottom: { background: 'url(/images/cube/bottom.svg)' },
+    }
+}
+
+// alias for styles
+const s = styles
 
 main()
 function main() {
-	const footerHeight = 44.5
-
-	class App extends React.Component {
-		render() {
-			return (
-				<i-scene ref="scene">
-					<i-node size-mode="proportional literal" size="1 1" ref="titleArea">
-						<i-node size-mode="proportional proportional" size="1 1">
-							<MorphingColorSpiral />
-						</i-node>
-
-						<i-node
-							class="gradient-background"
-							size-mode="proportional proportional"
-							size="1 1"
-							opacity="0.85"
-						></i-node>
-
-						<i-node
-							id="titleNode"
-							class="centerText"
-							size-mode="proportional literal"
-							size="1 50"
-							align="0 0.5"
-							mount-point="0 0.5"
-						>
-							<h1>INFAMOUS</h1>
-						</i-node>
-					</i-node>
-
-					<i-node
-						id="footerNode"
-						class="centerText"
-						size-mode="proportional literal"
-						size={`1 ${footerHeight}`}
-						align="0 1"
-						mount-point="0 1"
-					>
-						<i-node
-							class="centerText"
-							size-mode="proportional literal"
-							size="0.333333 20"
-							align="0 0.5"
-							mount-point="0 0.5"
-						>
-							<a href="http://forums.infamous.io">Discussion</a>
-						</i-node>
-
-						<i-node
-							class="centerText"
-							size-mode="proportional literal"
-							size="0.333333 20"
-							align="0.333333 0.5"
-							mount-point="0 0.5"
-						>
-							<a href="/docs/index.html">Docs</a>
-						</i-node>
-
-						<i-node
-							class="centerText"
-							size-mode="proportional literal"
-							size="0.333333 20"
-							align="0.666666 0.5"
-							mount-point="0 0.5"
-						>
-							<a href="http://github.com/trusktr/infamous">GitHub</a>
-						</i-node>
-					</i-node>
-				</i-scene>
-			)
-		}
-
-		updateSize() {
-			const {titleArea, scene} = this.refs
-			const sceneHeight = scene.calculatedSize.y
-			let newHeight = sceneHeight - footerHeight
-
-			// TODO make a helpful warning about negative sizes in lume
-			if (newHeight < 0) {
-				console.warn('Size can not be negative. Setting it to zero.')
-				newHeight = 0
-			}
-
-			titleArea.size.y = newHeight
-		}
-
-		componentDidMount() {
-			this.updateSize()
-			this.refs.scene.on('sizechange', () => this.updateSize())
-		}
-	}
-
 	const root = document.createElement('div')
 	root.id = 'root' // needed for styling
 	document.body.append(root)
