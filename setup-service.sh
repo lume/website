afa1b5fae92d4d4180b0af172995e7a2cfae193c
Joe Pea <joe@trusktr.io>
2020-02-10 17:13:58 -0800

    update systemd service

--- setup-service.sh
+++ setup-service.sh
@@ -1,3 +1,3 @@
-sudo cp -f /home/infamous/infamous/website/infamous-website.service /etc/systemd/system/
+sudo ln -sf /home/infamous/lume+umbrella/apps/website/systemd.service /etc/systemd/system/infamous-website.service
 sudo systemctl daemon-reload
 sudo systemctl enable infamous-website.service
