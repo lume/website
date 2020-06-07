bceb2e6fc63af93d859dea4d3562938929c989b4
Joe Pea <joe@trusktr.io>
2020-06-06 21:25:01 -0700

    update the systemd service to, rename infamous to lume

--- setup-service.sh
+++ setup-service.sh
@@ -1,3 +1,3 @@
-sudo ln -sf /home/infamous/lume+umbrella/apps/website/systemd.service /etc/systemd/system/infamous-website.service
+sudo ln -sf /home/infamous/lume+umbrella/apps/website/systemd.service /etc/systemd/system/lume-website.service
 sudo systemctl daemon-reload
-sudo systemctl enable infamous-website.service
+sudo systemctl enable lume-website.service
