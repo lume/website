cd96e03aa2d158528cf578bf67852f8582e4502f
Joe Pea <joe@trusktr.io>
2020-06-26 10:56:28 -0700

    update the systemd service with new naming

--- setup-service.sh
+++ setup-service.sh
@@ -1,3 +1,3 @@
-sudo ln -sf /home/infamous/lume+umbrella/apps/website/systemd.service /etc/systemd/system/lume-website.service
+sudo cp -f ./systemd.service /etc/systemd/system/lume-website.service
 sudo systemctl daemon-reload
 sudo systemctl enable lume-website.service
